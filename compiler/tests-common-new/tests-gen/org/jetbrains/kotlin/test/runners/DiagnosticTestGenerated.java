31a3a1ea71103afb70d4fbb69fc27529d46d72d8
Anastasiya Shadrina <a.shadrina5@mail.ru>
2020-08-20 14:11:50 +0700

    [FE] Support context receivers on classes

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/DiagnosticTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/DiagnosticTestGenerated.java
@@ -9457,6 +9457,12 @@ public class DiagnosticTestGenerated extends AbstractDiagnosticTest {
                 }
 
                 @Test
+                @TestMetadata("genericOuterClass.kt")
+                public void testGenericOuterClass() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/extensions/contextReceivers/genericOuterClass.kt");
+                }
+
+                @Test
                 @TestMetadata("insideDeclaration.kt")
                 public void testInsideDeclaration() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/extensions/contextReceivers/insideDeclaration.kt");
@@ -9478,6 +9484,12 @@ public class DiagnosticTestGenerated extends AbstractDiagnosticTest {
                 @TestMetadata("noExplicitReceiver.kt")
                 public void testNoExplicitReceiver() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/extensions/contextReceivers/noExplicitReceiver.kt");
+                }
+
+                @Test
+                @TestMetadata("outerClass.kt")
+                public void testOuterClass() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/extensions/contextReceivers/outerClass.kt");
                 }
 
                 @Test
