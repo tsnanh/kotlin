d1b6fee22b27a9755dd0e0c478126de5f2a66060
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2021-03-16 15:25:16 +0100

    Add file exists/does not exist assertions.
    ^KT-45744 In Progress

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/fileAssertions.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.testbase
+
+import java.nio.file.Files
+
+/**
+ * Asserts file under [pathToFile] relative to the test project exists.
+ */
+fun TestProject.assertFileExists(
+    pathToFile: String
+) {
+    val filePath = projectPath.resolve(pathToFile)
+    assert(Files.exists(filePath)) {
+        "File '${filePath}' does not exist!"
+    }
+}
+
+/**
+ * Asserts file under [pathToFile] relative to the test project does not exist.
+ */
+fun TestProject.assertFileNotExists(
+    pathToFile: String
+) {
+    val filePath = projectPath.resolve(pathToFile)
+    assert(!Files.exists(filePath)) {
+        "File '${filePath}' exists!"
+    }
+}
