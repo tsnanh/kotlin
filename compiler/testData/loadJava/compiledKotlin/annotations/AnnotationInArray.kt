2168ab8bf3e5f5ff480b7275fa9ec5f0cfaf4dc4
Ting-Yuan Huang <laszio@google.com>
2021-03-25 22:34:47 -0700

    Support array of annotation in KotlinJvmBinaryClass
    KotlinJvmBinaryClass.AnnotationArrayArgumentVisitor didn't cover the
    case when the element type is an Annotation. Therefore, when the
    compiler read an array of annotations from JVM binary classes built from
    Kotlin sources, it got an empty array regardless of what was written in
    the bytecode.
    
    For example, Foo.value below is read as an empty array when SomeClass
    resides in another Kotlin module.
    
      @Foo(
        value = [Bar(1), Bar(2)]
      )
      class SomeClass

---
+++ compiler/testData/loadJava/compiledKotlin/annotations/AnnotationInArray.kt
@@ -0,0 +1,14 @@
+package test
+
+annotation class Anno(
+    val value: Array<Bnno>
+)
+
+annotation class Bnno(
+    val value: String
+)
+
+@Anno(
+    value = [Bnno("x"), Bnno("y")]
+)
+public class AnnotationInArray
