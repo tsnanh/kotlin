85e6a34250d03b5ad007e70b0351697e80293f6f
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-04-01 17:56:48 +0200

    [REMOVE ME]: Commit adding some code for testing a sample project [REMOVE ME]

---
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/dsl/SourceSetHierarchyBuilder.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.dsl
+
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+
+
+operator fun KotlinSourceSet.invoke(builder: SourceSetHierarchyBuilder.() -> Unit): KotlinSourceSet {
+    SourceSetHierarchyBuilder(this).builder()
+    return this
+}
+
+class SourceSetHierarchyBuilder(private val node: KotlinSourceSet) {
+    operator fun KotlinSourceSet.unaryMinus() {
+        this.dependsOn(node)
+    }
+}
