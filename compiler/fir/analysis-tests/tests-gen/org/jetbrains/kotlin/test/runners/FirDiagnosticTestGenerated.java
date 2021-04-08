eb2f2cbb4f3f59b9f717911ff2a80a72fd72ceef
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2021-04-08 16:04:10 +0300

    ~~~ Failing FIR test ~~~

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirDiagnosticTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirDiagnosticTestGenerated.java
@@ -3108,6 +3108,12 @@ public class FirDiagnosticTestGenerated extends AbstractFirDiagnosticTest {
             }
 
             @Test
+            @TestMetadata("symbolsAndDescriptors.kt")
+            public void testSymbolsAndDescriptors() throws Exception {
+                runTest("compiler/fir/analysis-tests/testData/resolve/problems/symbolsAndDescriptors.kt");
+            }
+
+            @Test
             @TestMetadata("transform.kt")
             public void testTransform() throws Exception {
                 runTest("compiler/fir/analysis-tests/testData/resolve/problems/transform.kt");
