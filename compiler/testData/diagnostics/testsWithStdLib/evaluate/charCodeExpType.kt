79c5e6050c83c9a22133afb5c60c8e4701b1418a
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2021-04-09 06:30:02 +0300

    Do not consider Char.code as a pure integer constant

--- compiler/testData/diagnostics/testsWithStdLib/evaluate/charCodeExpType.kt
+++ compiler/testData/diagnostics/testsWithStdLib/evaluate/charCodeExpType.kt
@@ -3,6 +3,6 @@ const val p2: Long = '\n'.code.toLong()
 const val p3: Byte = '\n'.code.toByte()
 const val p4: Short = '\n'.code.toShort()
 
-const val e2: Long =  <!TYPE_MISMATCH!>'\n'.code<!>
+const val e2: Long = <!TYPE_MISMATCH!>'\n'.code<!>
 const val e3: Byte = <!TYPE_MISMATCH!>'\n'.code<!>
 const val e4: Short = <!TYPE_MISMATCH!>'\n'.code<!>
